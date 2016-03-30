40a9cea2243cfe5ac935c5c5175c48a99ff5946f
djpillen@umich.edu <djpillen@umich.edu>
2016-03-30 12:50:03 -0400

    slight modification to extent type

--- backend/model/bhl_ead_exporter.rb
+++ backend/model/bhl_ead_exporter.rb
@@ -549,7 +549,7 @@ class EADSerializer < ASpaceExport::Serializer
         if extent_number_float == 1.0
           extent_type = SingularizeExtents.singularize_extent(extent_type)
         end
-        extent_number_and_type = "#{e['number']} #{extent_type}"
+        extent_number_and_type = "#{e['number']} #{I18n.t('enumerations.extent_extent_type.'+e['extent_type'], :default => e['extent_type'])}"
         physical_details = []
         physical_details << e['container_summary'] if e['container_summary']
         physical_details << e['physical_details'] if e['physical_details']
