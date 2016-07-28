fbb50386fc33540d3ba6e6f8c03ab5375d716b75
djpillen@umich.edu <djpillen@umich.edu>
2016-07-28 10:13:36 -0400

    modify eadheader for pdf exports

--- backend/model/mixins/ead_serializer_customizations.rb
+++ backend/model/mixins/ead_serializer_customizations.rb
@@ -2,6 +2,11 @@ module EADSerializerCustomizations
 
   def self.included(base)
     base.class_eval do
+
+      def serialize_ead_header(data, xml, fragments)
+        modified_serialize_eadheader(data, xml, fragments)
+      end
+
       def serialize_extents(data, xml, fragments)
         modified_serialize_extents(data, xml, fragments)
       end
@@ -9,7 +14,129 @@ module EADSerializerCustomizations
       def serialize_controlaccess(data, xml, fragments)
         modified_serialize_controlaccess(data, xml, fragments)
       end
+
     end
+  end
+
+  def modified_serialize_eadheader(data, xml, fragments)
+    eadheader_atts = {:findaidstatus => data.finding_aid_status,
+                      :repositoryencoding => "iso15511",
+                      :countryencoding => "iso3166-1",
+                      :dateencoding => "iso8601",
+                      :langencoding => "iso639-2b"}.reject{|k,v| v.nil? || v.empty? || v == "null"}
+
+    xml.eadheader(eadheader_atts) {
+
+      eadid_atts = {:countrycode => data.repo.country,
+              :url => data.ead_location,
+              :mainagencycode => data.mainagencycode}.reject{|k,v| v.nil? || v.empty? || v == "null" }
+
+      xml.eadid(eadid_atts) {
+        xml.text data.ead_id
+      }
+
+      xml.filedesc {
+
+        xml.titlestmt {
+
+          titleproper = ""
+          titleproper += "#{data.finding_aid_title} " if data.finding_aid_title
+          titleproper += "#{data.title}" if ( data.title && titleproper.empty? )
+          #titleproper += "<num>#{(0..3).map{|i| data.send("id_#{i}")}.compact.join('.')}</num>"
+          xml.titleproper("type" => "filing") { sanitize_mixed_content(data.finding_aid_filing_title, xml, fragments)} unless data.finding_aid_filing_title.nil?
+          xml.titleproper {  sanitize_mixed_content(titleproper, xml, fragments) }
+          xml.subtitle {  sanitize_mixed_content(data.finding_aid_subtitle, xml, fragments) } unless data.finding_aid_subtitle.nil?
+          xml.author { sanitize_mixed_content(data.finding_aid_author, xml, fragments) }  unless data.finding_aid_author.nil?
+          xml.sponsor { sanitize_mixed_content( data.finding_aid_sponsor, xml, fragments) } unless data.finding_aid_sponsor.nil?
+
+        }
+
+        unless data.finding_aid_edition_statement.nil?
+          xml.editionstmt {
+            sanitize_mixed_content(data.finding_aid_edition_statement, xml, fragments, true )
+          }
+        end
+
+        xml.publicationstmt {
+          xml.publisher { sanitize_mixed_content(data.repo.name,xml, fragments) }
+
+          if data.repo.image_url
+            xml.p ( { "id" => "logostmt" } ) {
+              xml.extref ({"xlink:href" => data.repo.image_url,
+                          "xlink:actuate" => "onLoad",
+                          "xlink:show" => "embed",
+                          "xlink:type" => "simple"
+                          })
+                          }
+          end
+          if (data.finding_aid_date)
+            xml.p {
+                  val = data.finding_aid_date
+                  xml.date {   sanitize_mixed_content( val, xml, fragments) }
+                  }
+          end
+
+          unless data.addresslines.empty?
+            xml.address {
+              data.addresslines.each do |line|
+                xml.addressline { sanitize_mixed_content( line, xml, fragments) }
+              end
+              if data.repo.url
+                xml.addressline ( "URL: " ) {
+                  xml.extptr ( {
+                          "xlink:href" => data.repo.url,
+                          "xlink:title" => data.repo.url,
+                          "xlink:type" => "simple",
+                          "xlink:show" => "new"
+                          } )
+                 }
+              end
+            }
+          end
+        }
+
+        if (data.finding_aid_series_statement)
+          val = data.finding_aid_series_statement
+          xml.seriesstmt {
+            sanitize_mixed_content(  val, xml, fragments, true )
+          }
+        end
+        if ( data.finding_aid_note )
+            val = data.finding_aid_note
+            xml.notestmt { xml.note { sanitize_mixed_content(  val, xml, fragments, true )} }
+        end
+
+      }
+
+      xml.profiledesc {
+        creation = "This finding aid was produced using ArchivesSpace on <date>#{Time.now}</date>."
+        xml.creation {  sanitize_mixed_content( creation, xml, fragments) }
+
+        if (val = data.finding_aid_language)
+          xml.langusage (fragments << val)
+        end
+
+        if (val = data.descrules)
+          xml.descrules { sanitize_mixed_content(val, xml, fragments) }
+        end
+      }
+
+      if data.revision_statements.length > 0
+        xml.revisiondesc {
+          data.revision_statements.each do |rs|
+              if rs['description'] && rs['description'].strip.start_with?('<')
+                xml.text (fragments << rs['description'] )
+              else
+                xml.change {
+                  rev_date = rs['date'] ? rs['date'] : ""
+                  xml.date (fragments <<  rev_date )
+                  xml.item (fragments << rs['description']) if rs['description']
+                }
+              end
+          end
+        }
+      end
+    }
   end
 
   def modified_serialize_extents(obj, xml, fragments)
