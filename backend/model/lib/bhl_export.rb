08ec3c5786368233e7e36061d7cd9896060be3f3
djpillen@umich.edu <djpillen@umich.edu>
2017-06-22 15:00:39 -0400

    additional modifications for ASpace v2

--- backend/model/lib/bhl_export.rb
+++ backend/model/lib/bhl_export.rb
@@ -6,14 +6,19 @@ module BhlExportHelpers
   ASpaceExport::init
 
   def generate_bhl_ead(id, include_unpublished, include_daos, use_numbered_c_tags)
-    obj = resolve_references(Resource.to_jsonmodel(id), ['repository', 'linked_agents', 'subjects', 'tree', 'digital_object', 'top_container'])
+    resolve = ['repository', 'linked_agents', 'subjects', 'digital_object', 'top_container', 'top_container::container_profile']
+
+    resource = Resource.get_or_die(id)
+
+    jsonmodel = JSONModel(:resource).new(resolve_references(Resource.to_jsonmodel(resource), resolve))
+
     opts = {
       :include_unpublished => include_unpublished,
       :include_daos => include_daos,
       :use_numbered_c_tags => use_numbered_c_tags,
       :contains_university_restrictions => contains_university_restrictions(id)
     }
-    ead = ASpaceExport.model(:bhl_ead).from_resource(JSONModel(:resource).new(obj), opts)
+    ead = ASpaceExport.model(:bhl_ead).from_resource(jsonmodel, resource.tree(:all, mode = :sparse), opts)
     ASpaceExport::stream(ead)
   end
 
@@ -34,13 +39,18 @@ module BhlExportHelpers
   end
 
   def generate_digitization_ead(id, include_unpublished, include_daos, use_numbered_c_tags)
-    obj = resolve_references(Resource.to_jsonmodel(id), ['repository', 'linked_agents', 'subjects', 'tree', 'digital_object', 'top_container'])
+    resolve = ['repository', 'linked_agents', 'subjects', 'digital_object', 'top_container', 'top_container::container_profile']
+
+    resource = Resource.get_or_die(id)
+
+    jsonmodel = JSONModel(:resource).new(resolve_references(Resource.to_jsonmodel(resource), resolve))
+
     opts = {
       :include_unpublished => include_unpublished,
       :include_daos => include_daos,
       :use_numbered_c_tags => use_numbered_c_tags
     }
-    ead = ASpaceExport.model(:bhl_digitization).from_resource(JSONModel(:resource).new(obj), opts)
+    ead = ASpaceExport.model(:bhl_digitization).from_resource(jsonmodel, resource.tree(:all, mode = :sparse), opts)
     ASpaceExport::stream(ead)
   end
 
